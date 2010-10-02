6fe9de764b1f91f31919d8409e2ed8d1fdf4f81b
blueprintllc <blueprint.partnership@gmail.com>
2010-10-02 18:57:12 -0400

    no index

--- db/migrate/20100927042838_add_stock_id_to_ideas.rb
+++
@@ -1,9 +0,0 @@
-class AddStockIdToIdeas < ActiveRecord::Migration
-  def self.up
-    add_column :ideas, :stock_id, :integer
-  end
-
-  def self.down
-    remove_column :ideas, :stock_id
-  end
-end
--- db/migrate/20100927042838_add_ticker_id_to_ideas.rb
+++
@@ -1,9 +0,0 @@
-class AddTickerIdToIdeas < ActiveRecord::Migration
-  def self.up
-    add_column :ideas, :ticker_id, :integer
-  end
-
-  def self.down
-    remove_column :ideas, :ticker_id
-  end
-end
